896d46288faddf1a767f577bf399394139474fb6
taralovestea <59029999+taralovestea@users.noreply.github.com>
2020-02-14 20:31:41 -0600

    added fswritefile functionality

---
+++ index.js
@@ -0,0 +1,88 @@
+const inquirer = require("inquirer");
+const axios= require("axios");
+const fs = require("fs")
+
+
+const questions = [
+{
+    type: "input",
+    name: "username",
+    message: "What is your github username?"
+},
+{
+    type: "input",
+    name: "project",
+    message: "What is your project title?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "What is a description of your project?"
+},
+{
+    type: "input",
+    name: "contents",
+    message: "What is the table of contents"
+},
+{
+    type: "list",
+    name: "installation",
+    choices: ["cdRom", "zip", "magic"]
+},
+{
+    type: "input",
+    name: "usage",
+    message: "what is the use???"
+},
+{
+    type: "input",
+    name: "license",
+    message: "what is the license??"
+},
+{
+    type: "input",
+    name: "test",
+    message: "what is the test??"
+},
+
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(){
+        console.log("Success")
+    })
+}
+
+function init() {
+    inquirer.prompt(questions).then(function(data){
+        // console.log(data)
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+
+        axios
+        .get (queryUrl)
+        .then(function(res) {
+            var image= (res.data.avatar_url)
+            var bio= (res.data.bio)
+
+            let combinedData = {
+                ...data,
+                image: image,
+                bio: bio,
+
+            }
+            console.log(combinedData)
+
+            let text = `
+            ${combinedData.bio}
+
+            `
+
+
+            writeToFile(res.data.login + ".txt", text )
+        })
+
+    })
+
+
+}
+    init();
